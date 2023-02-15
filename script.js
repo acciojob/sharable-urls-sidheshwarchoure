c78fda259b79a5dd09cd9ee9a1300a3922d5b36e
sidheshwarchoure <92964594+sidheshwarchoure@users.noreply.github.com>
2023-02-15 12:33:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // your code here
+var form = document.querySelector("form");
+var h3 = document.querySelector("h3");
+var nameElement = document.querySelector("#name");
+var yearElement = document.querySelector("#year");
+var buttonElement = document.querySelector("#button");
+
+function handleSubmit(event){
+	event.preventDefault();
+
+	var h3Value = "https://localhost:8080/";
+	var name = nameElement.value;
+	var year = yearElement.value;
+
+	if(name && year){
+		h3Value += `?name=` + name + `&year=` + year;
+	}
+	else if(!name && year){
+		h3Value += `?year=` + year;
+	}
+	else if(name && !year)
+	{
+		h3Value += `&name` + name;
+	}
+	h3textContent = h3Value.value;
+}
+form.addEventListener('Submit',handleSubmit);
